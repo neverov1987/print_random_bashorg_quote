f83f10db8b9dadaa59aab0fe0b8cc9ace09c1463
Mikhail <mikhail.neverov@pimpay.ru>
2022-10-27 23:10:39 +0300

    cleanup

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ url = 'http://bashorg.org/casual'
 
 page = requests.get(url)
 tree = html.fromstring(page.content)
-# hr = tree.xpath('//div[@class="number"]/text()')
 hr = tree.xpath('//*[@id="quotes"]/div[1]/div[2]//text()')
-# tree.xpath("//div[@class='exchangeRates']/table/tr[5]/td[4]//text()")
 for i in range(len(hr)):
     print(hr[i])
 
